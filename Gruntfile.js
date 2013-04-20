8e9d01ba7538a82a894a568de5c9a853ed74c012
Beau Charman <beau.charman@gmail.com>
2013-04-20 19:56:05 +1000

    Double Quotes

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,8 +252,8 @@ module.exports = function(grunt)
     {
       deploy:
       {
-        src: ['application/development/filename.html'],
-        dest: 'application/deploy/filename.html',
+        src: ["application/development/filename.html"],
+        dest: "application/deploy/filename.html",
         replacements:
         [
           {
