4ddc8bf74b8242cb5adb4bdedb34b2489a077888
Beau Charman <beau.charman@gmail.com>
2013-05-12 09:43:18 +1000

    Added clean task and updated comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /**
  * Project Name
- * ------------------------------------------------------------------------
+ * ========================================================================
  * Gruntfile.js
  * @version   2.0 | 28th April, 2013
  * @author    Beau Charman | @beaucharman | http://beaucharman.me
@@ -11,29 +11,29 @@
  * npm uninstall -g grunt
  * npm install -g grunt-cli
  * npm install grunt --save-dev
- * ------------------------------------------------------------------------ */
+ * ======================================================================== */
 
-module.exports = function( grunt ) {
+'use strict';
 
-  'use strict';
+module.exports = function(grunt) {
 
-  /* ------------------------------------------------------------------------
+  /* ========================================================================
      Init configuration
-     ------------------------------------------------------------------------ */
+     ======================================================================== */
   grunt.initConfig({
 
     /**
      * Package file
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * All variable, dependency and version information for the project.
-     * ------------------------------------------------------------------------ */
-    pkg: grunt.file.readJSON( "package.json" ),
+     * ======================================================================== */
+    pkg: grunt.file.readJSON("package.json"),
 
     /**
      * Sass
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * npm install grunt-contrib-sass --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     sass:
     {
       development:
@@ -64,9 +64,9 @@ module.exports = function( grunt ) {
 
     /**
      * cssLint
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * npm install grunt-contrib-csslint --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     csslint:
     {
       all:
@@ -80,9 +80,9 @@ module.exports = function( grunt ) {
 
     /**
      * Coffee
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * npm install grunt-contrib-coffee --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     coffee:
     {
       deploy:
@@ -100,9 +100,9 @@ module.exports = function( grunt ) {
 
     /**
      * Concat
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * npm install grunt-contrib-concat --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     concat:
     {
       deploy:
@@ -118,9 +118,9 @@ module.exports = function( grunt ) {
 
     /**
      * Uglify
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * npm install grunt-contrib-uglify --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     uglify:
     {
       deploy:
@@ -142,9 +142,9 @@ module.exports = function( grunt ) {
 
     /**
      * jsHint
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * npm install grunt-contrib-jshint --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     jshint:
     {
       all:
@@ -155,10 +155,10 @@ module.exports = function( grunt ) {
     },
 
     /**
-     * Jekyll Task
-     * ------------------------------------------------------------------------
+     * Jekyll
+     * ========================================================================
      * npm install grunt-jekyll --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     jekyll:
     {
       server:
@@ -181,12 +181,23 @@ module.exports = function( grunt ) {
     },
 
     /**
-     * Copy Files
-     * ------------------------------------------------------------------------
+     * Clean
+     * ========================================================================
+     * npm install grunt-contrib-clean --save-dev
+     * ======================================================================== */
+    clean:
+    {
+      deploy: ["<%= pkg.path.deploy %>"],
+      wordpress: ["<%= pkg.path.wordpress %>"]
+    },
+
+    /**
+     * Copy
+     * ========================================================================
      * npm install grunt-contrib-copy --save-dev
      * For WordPress theme development, use:
      * copy.build.files.dest:"<%= pkg.path.wordpress %>"
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     copy:
     {
       build:
@@ -196,8 +207,8 @@ module.exports = function( grunt ) {
           {
             expand: true,
             cwd:    "<%= pkg.path.development %>",
-            src:    [ "**", "!**.DS_Store" ],
-            dest:   "<%= pkg.path.deploy %>" //relative/path/to/destination/
+            src:    ["**", "!**.DS_Store"],
+            dest:   "<%= pkg.path.deploy %>"
           }
         ]
       },
@@ -208,7 +219,7 @@ module.exports = function( grunt ) {
           {
             expand: true,
             cwd:    "<%= pkg.path.development %>",
-            src:    [ "**", "!**.DS_Store" ],
+            src:    ["**", "!**.DS_Store"],
             dest:   "<%= pkg.path.wordpress %>"
           }
         ]
@@ -220,7 +231,7 @@ module.exports = function( grunt ) {
           {
             expand: true,
             cwd:    "<%= pkg.path.development %>",
-            src:    [ "**", "!**.DS_Store" ],
+            src:    ["**", "!**.DS_Store"],
             dest:   "<%= pkg.path.deploy %>"
           }
         ]
@@ -232,7 +243,7 @@ module.exports = function( grunt ) {
           {
             expand: true,
             cwd:    "<%= pkg.path.deploy %>",
-            src:    [ "**" ],
+            src:    ["**"],
             dest:   "relative/path/to/destination/"
           }
         ]
@@ -241,10 +252,10 @@ module.exports = function( grunt ) {
 
     /**
      * FTP Deploy
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * git clone git://github.com/zonak/grunt-ftp-deploy.git
      * Store ftp connection details in a .ftppass file
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     "ftp-deploy":
     {
       deploy:
@@ -257,20 +268,20 @@ module.exports = function( grunt ) {
         },
         src:        "<%= pkg.path.deploy %>",
         dest:       "public_html/",
-        exclusions: [ "**/.DS_Store", "**/Thumbs.db" ]
+        exclusions: ["**/.DS_Store", "**/Thumbs.db"]
       }
     },
 
     /**
      * Text Replace
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * npm install grunt-text-replace --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     replace:
     {
       deploy:
       {
-        src:  [ "<%= pkg.path.development %>filename.html" ],
+        src:  ["<%= pkg.path.development %>filename.html"],
         dest: "<%= pkg.path.deploy %>filename.html",
         replacements:
         [
@@ -284,9 +295,9 @@ module.exports = function( grunt ) {
 
     /**
      * Smushit
-     * ------------------------------------------------------------------------
+     * ========================================================================
      * npm install grunt-smushit
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     smushit:
     {
       deploy:
@@ -300,16 +311,16 @@ module.exports = function( grunt ) {
     },
 
     /**
-     * Contrib Watch Task
-     * ------------------------------------------------------------------------
+     * Watch
+     * ========================================================================
      * npm install grunt-contrib-watch --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     watch:
     {
       sass:
       {
-        files: [ "<%= pkg.path.src.sass %>**" ],
-        tasks: [ "sass:development" ],
+        files: ["<%= pkg.path.src.sass %>**"],
+        tasks: ["sass:development"],
         options:
         {
           nospawn: true
@@ -318,8 +329,8 @@ module.exports = function( grunt ) {
       // used with event listener
       event:
       {
-        files: [ "<%= pkg.path.development %>**" ],
-        tasks: [ "copy:changed" ],
+        files: ["<%= pkg.path.development %>**"],
+        tasks: ["copy:changed"],
         options:
         {
           nospawn: true
@@ -328,17 +339,17 @@ module.exports = function( grunt ) {
     },
 
     /**
-     * Regarde Watch Task
-     * ------------------------------------------------------------------------
+     * Regarde
+     * ========================================================================
      * Watch task alternative.
      * npm install grunt-regarde --save-dev
-     * ------------------------------------------------------------------------ */
+     * ======================================================================== */
     regarde:
     {
       sass:
       {
         files:  "<%= pkg.path.src.sass %>**",
-        tasks:  [ "sass:development" ],
+        tasks:  ["sass:development"],
         events: true
       }
     }
@@ -346,16 +357,16 @@ module.exports = function( grunt ) {
   });
 
   /** Dynamic Watch task
-   * ------------------------------------------------------------------------
+   * ========================================================================
    * To be used with Contrib Watch Task, this event function
    * will capture the actual file changed and run run the task
    * on it, rather then the entire watched folder.
-   * ------------------------------------------------------------------------ */
-  grunt.event.on( "watch", function( event, file ) {
-    var pkg = grunt.file.readJSON( "package.json" );
+   * ======================================================================== */
+  grunt.event.on("watch", function (event, file) {
+    var pkg = grunt.file.readJSON("package.json");
     var cwd = pkg.path.development;
-    var filepath = file.replace( cwd, "" );
-    grunt.config.set( "copy",
+    var filepath = file.replace(cwd, "");
+    grunt.config.set("copy",
     {
       changed:
       {
@@ -372,18 +383,18 @@ module.exports = function( grunt ) {
 
   /**
    * Volo
-   * ------------------------------------------------------------------------
+   * ========================================================================
    * npm install grunt-volo
    * Used to fetch Github repos through the terminal.
    * To fetch a file:  grunt volo:add:[flags]:[archive]:[localName]
    * To search github: grunt volo:search:[name]
    * More information:
    *   https://github.com/volojs/volo/blob/master/commands/add/doc.md
-   * ------------------------------------------------------------------------ */
+   * ======================================================================== */
 
-  /* ------------------------------------------------------------------------
+  /* ========================================================================
      Load Tasks
-     ------------------------------------------------------------------------ */
+     ======================================================================== */
   //grunt.loadNpmTasks("grunt-contrib-sass");
   //grunt.loadNpmTasks('grunt-contrib-csslint');
   //grunt.loadNpmTasks("grunt-contrib-coffee");
@@ -391,6 +402,7 @@ module.exports = function( grunt ) {
   //grunt.loadNpmTasks("grunt-contrib-uglify");
   //grunt.loadNpmTasks('grunt-contrib-jshint');
   //grunt.loadNpmTasks("grunt-jekyll");
+  //grunt.loadNpmTasks("grunt-contrib-clean");
   //grunt.loadNpmTasks("grunt-contrib-copy");
   //grunt.loadNpmTasks("grunt-ftp-deploy");
   //grunt.loadNpmTasks("grunt-text-replace");
@@ -399,43 +411,45 @@ module.exports = function( grunt ) {
   //grunt.loadNpmTasks("grunt-regarde");
   //grunt.loadNpmTasks('grunt-volo');
 
-  /* ------------------------------------------------------------------------
+  /* ========================================================================
      Register custom tasks
-     ------------------------------------------------------------------------ */
+     ======================================================================== */
 
   /* The default Grunt task
-   * ------------------------------------------------------------------------
-   * Tasks run simply with the command `grunt`
+   * ========================================================================
+   * Tasks to run simply with the command `grunt`
    */
-  grunt.registerTask( "default", [ "sass:development" ] );
+  grunt.registerTask("default", ["sass:development"]);
 
   /** Watch Sass
-   * ------------------------------------------------------------------------
+   * ========================================================================
    * Simply watch for Sass changes, and process
    */
-  grunt.registerTask( "watchSass", [ "watch:sass" ] );
+  grunt.registerTask("watchSass", ["watch:sass"]);
 
   /** Build
-   * ------------------------------------------------------------------------
+   * ========================================================================
    * Run preprocessing and copy files
    */
-  grunt.registerTask( "build", [ "sass:development", "copy:build" ] );
+  grunt.registerTask("build", ["sass:development", "copy:build"]);
 
   /** Deploy
-   * ------------------------------------------------------------------------
+   * ========================================================================
    * Run preprocessing, concatenate, minify and copy files for deploment
    */
-  grunt.registerTask( "deploy", [ "uglify:deploy", "copy:deploy", "sass:deploy" ] );
+  grunt.registerTask("deploy", ["uglify:deploy", "copy:deploy", "sass:deploy"]);
 
   /** WordPress Deploy
-   * ------------------------------------------------------------------------
+   * ========================================================================
    * Run preprocessing, concatenate, minify and copy files.
    * Also change any development mode options in template files to be
    * ready for deployment.
    */
-  grunt.registerTask( "wp-deploy", [ "copy:deploy", "uglify:deploy", "sass:deploy", "replace:deploy" ] );
+  grunt.registerTask("deploy", [
+    "clean:deploy", "copy:deploy", "uglify:deploy", "sass:deploy", "replace:deploy"
+  ]);
 
   /* Test deployment files */
-  grunt.registerTask( "testDeploy", [ "copy:testDeploy" ] );
+  grunt.registerTask("testDeploy", ["copy:testDeploy"]);
 
 };
