c816d9b17e1f48863bbd18b17efebb579ebe6c2a
Beau Charman <beau.charman@gmail.com>
2013-06-17 08:02:32 +1000

    Comment changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -478,11 +478,9 @@ module.exports = function(grunt) {
    * Also change any development mode options in template files to be
    * ready for deployment.
    */
-  /*
-  grunt.registerTask("deploy", [
-    "clean:deploy", "copy:deploy", "uglify:deploy", "sass:deploy", "replace:deploy"
-  ]);
-  */
+  //grunt.registerTask("deploy", [
+  //  "clean:deploy", "copy:deploy", "uglify:deploy", "sass:deploy", "replace:deploy"
+  //]);
 
   /* Test deployment files */
   grunt.registerTask("testDeploy", ["copy:testDeploy"]);
